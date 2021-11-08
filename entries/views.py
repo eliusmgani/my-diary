14a20ac5c7fbae73402bb28f23767568a80e5379
eliusmgani <eliasmgani@gmail.com>
2021-11-08 22:25:47 +0300

    authentication control

--- entries/views.py
+++ entries/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 from . models import Entry
 
 # Create your views here.
-class EntryListView(ListView):
+class LockedView(LoginRequiredMixin):
+    login_url = 'admin:login'
+
+class EntryListView(LockedView, ListView):
     model = Entry
     queryset = Entry.objects.all().order_by("-date_created")
 
-class EntryDetailView(DetailView):
+class EntryDetailView(LockedView, DetailView):
     model = Entry
 
-class EntryCreateView(CreateView):
+class EntryCreateView(LockedView, CreateView):
     model = Entry
     fields = ["title", "content"]
     success_url = reverse_lazy("entry-list")
 
-class EntryUpdateView(UpdateView):
+class EntryUpdateView(LockedView, UpdateView):
     model = Entry
     fields = ["title", "content"]
 
@@ -25,6 +32,29 @@ class EntryUpdateView(UpdateView):
             kwargs={"pk": self.entry.id}
         )
 
-class EntryDeleteView(DeleteView):
+class EntryDeleteView(LockedView, DeleteView):
     model = Entry
     success_url = reverse_lazy("entry-list")
+
+class EntryCreateView(LockedView, SuccessMessageMixin, CreateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_url = reverse_lazy("entry-list")
+    success_message = "Your new entry was created..!"
+
+class EntryUpdateView(LockedView, SuccessMessageMixin, UpdateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_message = "Your entry was update..!!"
+
+    def get_success_url(self):
+        return reverse_lazy("entry-detail", kwargs={"pk": self.object.pk})
+
+class EntryDeleteView(LockedView, DeleteView):
+    model = Entry
+    success_url = reverse_lazy("entry-list")
+    success_message = "Your entry was deleted..!!"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
